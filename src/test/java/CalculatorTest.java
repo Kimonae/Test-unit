ea9c52875a89bcdd4209bb77ee9a6b9cca79de2a
Kimonae <shinokasu@gmail.com>
2022-08-22 14:33:04 +0200

    update more beautiful

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,19 +1,22 @@
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Tests maths on calculator class")
 public class CalculatorTest {
 
+    @DisplayName("Test 4/2 =2")
     @Test
     void integerDivision() {
 
         //first unit test
         Calculator calculator = new Calculator();
-        int result = calculator.integerDivision(4,2);
-        int resulta = calculator.integerSoustract(8,2);
+        int result = calculator.integerDivision(4, 2);
+
         //alt + enter => import
-        assertEquals(2, result, "it's false");
-        assertEquals(4, result, "it's false");
+
+        assertEquals(2, result, "this division is false");
        /* assertNotEquals();
         assertNull();
         assertThrows();
@@ -21,4 +24,32 @@ public class CalculatorTest {
         assertTrue();
         assertFalse();*/
     }
+
+@DisplayName("Test 3-1 = 2")
+    @Test
+    void integerSoustract() {
+
+        //first unit test
+        Calculator calculator = new Calculator();
+        int number1 = 8;
+        int number2 = 5;
+
+        int resulta = calculator.integerSoustract(8, 2);
+        assertEquals(4, resulta, () -> number1 + "-" + number2 + "it's false");
+
+
+    }
+
+
+    //convention nommage
+    @Test
+    void testIntegerDivision_WhenFourDivisionDividedByTwo_ShouldReturnTwo() { }
+
+
+
+    @Test
+    void testIntegerSubstraction_When3minus1_ShouldReturnTwo() { }
+
+
+
 }
