c4b4f85535bf84c470caccd442b8dc98c0809642
Kimonae <shinokasu@gmail.com>
2022-08-22 14:58:26 +0200

    update 2

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -1,17 +1,42 @@
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("Tests maths on calculator class")
 public class CalculatorTest {
+ static Calculator calculator;
+    @BeforeAll
+    static void setup() {
+        System.out.println("Executing @BeforeAll method");
+    }
+
+    @AfterAll
+    static void cleanup() {
+        System.out.println("Excuting @Afterall method");
+
+    }
+
+    @BeforeEach
+    void beforeEachTestMethod() {
+        System.out.println("Excuting @BeforeEach method");
+
+    }
+    @AfterEach
+    void afterEachTestMethod() {
+        calculator = new Calculator();
+        System.out.println("Excuting @AfterEach method");
+
+
+    }
+
+
 
     @DisplayName("Test 4/2 =2")
     @Test
-    void integerDivision() {
+    void testIntegerDivision_WhenFourDivisionDividedByTwo_ShouldReturnTwo() {
 
         //first unit test
-        Calculator calculator = new Calculator();
+
         int result = calculator.integerDivision(4, 2);
 
         //alt + enter => import
@@ -27,14 +52,14 @@ public class CalculatorTest {
 
 @DisplayName("Test 3-1 = 2")
     @Test
-    void integerSoustract() {
+    void testIntegerSubstraction_When3minus1_ShouldReturnTwo() {
 
         //first unit test
-        Calculator calculator = new Calculator();
+
         int number1 = 8;
         int number2 = 5;
 
-        int resulta = calculator.integerSoustract(8, 2);
+        int resulta = calculator.integerSoustract(number1, number2);
         assertEquals(4, resulta, () -> number1 + "-" + number2 + "it's false");
 
 
@@ -42,13 +67,11 @@ public class CalculatorTest {
 
 
     //convention nommage
-    @Test
+   /* @Test
     void testIntegerDivision_WhenFourDivisionDividedByTwo_ShouldReturnTwo() { }
 
-
-
     @Test
-    void testIntegerSubstraction_When3minus1_ShouldReturnTwo() { }
+    void testIntegerSubstraction_When3minus1_ShouldReturnTwo() { }*/
 
 
 
